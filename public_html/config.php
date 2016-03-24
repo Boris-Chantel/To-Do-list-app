7788b534da0dea825a4389aeeb9c15a1cdbcaeb0
Matt Trask <mjftrask@gmail.com>
2016-03-23 21:59:31 -0400

    Update config.php

--- public_html/config.php
+++ public_html/config.php
@@ -1,11 +1,20 @@
-
 <?php
-	$user = root;
-	$pass = '';
-	$db = 'toDoList_Tasks';
+$user = root;
+$pass = '';
+$db = 'toDoList_Tasks';
 
-$conn = mysql_connect('localhost', $user, $pass, $db) or die ("unable to connect");
+$conn = mysql_connect('localhost', $user, $pass, $db);
 
-echo "Great Work!!!";
+if(!$conn) {
+	die ("unable to connect");
+}
 
-?>
+$results = mysql_query( // query goes here );
+
+if(!query) {
+	die('query could not be run');
+}
+
+$row = mysql_fetch_row($result);
+
+var_dump($row);
